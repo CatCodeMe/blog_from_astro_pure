f8e904b564aedbb4c62f3923957ab11b498c0394
hulinjiang <hulinjiang@58.com>
2025-03-28 11:55:48 +0800

    make 小花 ghibli style

--- src/site.config.ts
+++ src/site.config.ts
@@ -25,7 +25,7 @@ export const theme: ThemeUserConfig = {
   },
   /** Set a logo image to show in the homepage. */
   logo: {
-    src: 'src/assets/avatar.jpg',
+    src: 'src/assets/avatar.png',
     alt: 'Avatar'
   },
 
