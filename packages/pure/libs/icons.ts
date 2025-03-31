5a5adc48bda41f88b21564bdea00ba5af62122bb
hulinjiang <hulinjiang@58.com>
2025-03-31 14:45:17 +0800

    paper 组件调整, 使用图标代替左侧预览图

--- packages/pure/libs/icons.ts
+++ packages/pure/libs/icons.ts
@@ -174,6 +174,14 @@ export const BuiltInIcons = {
   close:
    '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16"><path fill="#f40c0c" fill-rule="evenodd" d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1m2.646 3.646L8 7.293L5.354 4.646l-.708.708L7.293 8l-2.647 2.646l.708.708L8 8.707l2.646 2.647l.708-.707L8.707 8l2.647-2.646z" clip-rule="evenodd" stroke-width="0.5" stroke="#f40c0c"/></svg>'
   , 
+
+  //paper.astro 的相关图标
+  paper_article:
+   '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 256 256"><path fill="currentColor" d="M52 136a4 4 0 0 1-4 4H24a4 4 0 0 1 0-8h12V60H24a4 4 0 0 1 0-8h16a4 4 0 0 1 3.39 1.87L80 112.45l36.61-58.57A4 4 0 0 1 120 52h16a4 4 0 0 1 0 8h-12v72h12a4 4 0 0 1 0 8h-24a4 4 0 0 1 0-8h4V70l-32.61 52.12a4 4 0 0 1-6.78 0L44 70v62h4a4 4 0 0 1 4 4m116-28h64a4 4 0 0 0 0-8h-64a4 4 0 0 0 0 8m64 24h-64a4 4 0 0 0 0 8h64a4 4 0 0 0 0-8m0 32H80a4 4 0 0 0 0 8h152a4 4 0 0 0 0-8m0 32H80a4 4 0 0 0 0 8h152a4 4 0 0 0 0-8" stroke-width="6.5" stroke="currentColor"/></svg>'
+   ,
+  paper_book: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path fill="currentColor" d="M18 2H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zM6 4h5v8l-2.5-1.5L6 12V4z"/>
+  </svg>`,
 }
 
 export const Icons = {
